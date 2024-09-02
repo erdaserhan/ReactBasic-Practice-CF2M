a4a628046cb3c4ef2fdb0ebf932f343f5033829b
erdaserhan <erhan209@gmail.com>
2024-09-02 12:16:25 +0200

    update item

--- src/components/TODOList.jsx
+++ src/components/TODOList.jsx
@@ -10,7 +10,10 @@ function Item({ item }) {
     return (
         <li id="{item?.id}" className="todo_item">
             <button className="todo_items_left">
-                
+                <svg>
+                    <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998" />
+                </svg>
+                <p>{ item?.title}</p>
             </button>
         </li>
     );
