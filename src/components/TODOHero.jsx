5e6c15adfe9614a1d62c576de00bfe73c2dcb033
erdaserhan <erhan209@gmail.com>
2024-09-02 11:55:14 +0200

    TODOHero ok

--- src/components/TODOHero.jsx
+++ src/components/TODOHero.jsx
@@ -1,3 +1,13 @@
 function TODOHero( { todos_completed, total_todos}) {
-    
+    return (
+        <section>
+            <div>
+                <p>Task Done</p>
+                <p>Keep it up</p>
+            </div>
+            <div>
+                {todos_completed}/{total_todos}
+            </div>
+        </section>
+    );
 }
